e7cd20df6f36351f134d1feea3da7adcc2c18408
kiran <kiran.k@motion.ac.in>
2024-06-03 17:53:41 +0530

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "mlapinpoint": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "mlapinpointf1c441cd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
