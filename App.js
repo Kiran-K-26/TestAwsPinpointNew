295ccf5660f930eca6d12d352d1189cbd86bea6f
kiran <kiran.k@motion.ac.in>
2024-06-19 17:40:21 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { View, StyleSheet, Button } from 'react-native';
-import { Analytics, Auth } from 'aws-amplify';
 
 const App = () => {
-
-  const myTokenReceivedHandler = (token) => {
-    // Do something with the received token
-    console.log("Device Token:-----------", deviceToken);
-    setDeviceToken(token);
-  };
-
-  const handleButtonClick = async () => {
-    // Update the endpoint
-    await Analytics.updateEndpoint({
-      address: 'kirankkittuchamy@gmail.com',
-      channelType: 'EMAIL',
-      optOut: 'NONE',
-      userAttributes: {
-        username: ['Kiran K']
-      },
-      userId: 'kirankkittuchamy@gmail.com',
-    })
-    // Send event
-    await Analytics.record({
-      name: 'TestMotionLearningApp',
-      attributes: { user_name: 'Boopathi Raja', user_id: '203510' },
-    });
-  }
 
 
   return (
     <View style={styles.container}>
-      <Button title="Press Me" onPress={() => { handleButtonClick() }} />
+      <Button title="Click Me" />
     </View>
   );
 };
